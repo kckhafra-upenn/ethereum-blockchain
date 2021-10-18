26891857e294a3d51e507bb35926ba36c9366994
kckhafra <kckhafra@gmail.com>
2021-10-18 17:56:12 -0400

    get gas fixed

--- ethereum_query.py
+++ ethereum_query.py
@@ -7,9 +7,8 @@ PORT='8545'
 w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
 
 if w3.isConnected():
-    print("x")
 #     This line will mess with our autograders, but might be useful when debugging
-#     print( "Connected to Ethereum node" )
+    print( "Connected to Ethereum node" )
 
 else:
     print( "Failed to connect to Ethereum node!" )
@@ -22,7 +21,8 @@ def get_transaction(tx):
 #   tx is the transaction
 def get_gas_price(tx):
     gas_price = 1 #YOUR CODE HERE
-    gas_price = w3.eth.generate_gas_price(tx)
+    gas_price = w3.eth.estimate_gas({'to':tx })
+    print("GP: ",gas_price)
     return gas_price
 
 def get_gas(tx):
